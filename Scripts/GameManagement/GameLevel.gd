a5d59f275332ca29438d78709df265f4be3a217b
Marco Dugatto <dughi.boogie@gmail.com>
2025-02-14 13:04:02 +0100

    Fixed web dialogues

--- Scripts/GameManagement/GameLevel.gd
+++ Scripts/GameManagement/GameLevel.gd
@@ -11,26 +11,13 @@ extends Node2D
 @export var triggered_dialogue_name: String
 
 
-var dialogue1 = ResourceLoader.load("res://Assets/Dialogues/AlarmTimeline.dtl")
-var dialogue2 = ResourceLoader.load("res://Assets/Dialogues/AssassinsTimeline.dtl")
-var dialogue3 = ResourceLoader.load("res://Assets/Dialogues/BossTimeline.dtl")
-var dialogue4 = ResourceLoader.load("res://Assets/Dialogues/DialogueStyle.tres")
-var dialogue5 = ResourceLoader.load("res://Assets/Dialogues/HammerDropTimeline.dtl")
-var dialogue6 = ResourceLoader.load("res://Assets/Dialogues/OpenPortalTimeline.dtl")
-var dialogue7 = ResourceLoader.load("res://Assets/Dialogues/RangedEnemiesTimeline.dtl")
-var dialogue8 = ResourceLoader.load("res://Assets/Dialogues/RecallTimeline.dtl")
-var dialogue9 = ResourceLoader.load("res://Assets/Dialogues/ThorTimeline.dtl")
-var dialogue10 = ResourceLoader.load("res://Assets/Dialogues/ThrowHammerTimeline.dtl")
-var dialogue11 = ResourceLoader.load("res://Assets/Dialogues/WalkingEnemiesTimeline.dtl")
-
-
 func _ready() -> void:
 	EventBus.connect(signal_name_trigger, trigger_dialogue)
 	EventBus.emit_signal("level_started", self)
 
 
 func trigger_dialogue(_fake_parameter = null):
-	pass
+	print("Triggered dialogue: " + triggered_dialogue_name)
 	
 	Dialogic.start(triggered_dialogue_name).process_mode = Node.PROCESS_MODE_ALWAYS
 	Dialogic.process_mode = Node.PROCESS_MODE_ALWAYS
